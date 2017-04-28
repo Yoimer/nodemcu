2f58de95f78c40e259f5982dfa9bde763abe12c1
Yoimer <ydrf19@gmail.com>
2017-04-28 19:09:07 -0400

    Added endOfLineReached(), CleanCurrentLine(), LastLineIsCMT()

--- nodemcu-sim800l/nodemcu-sim800l.ino
+++ nodemcu-sim800l/nodemcu-sim800l.ino
@@ -15,8 +15,35 @@ int currentLineIndex = 0;
 //line of serial output is the actual SMS message content
 bool nextLineIsMessage = false;
 
+////Boolean to be set to true if call notificaion was found and next line is NOT empty
+bool nextValidLineIsCall = false;
+
 //Boolean to be set to true if number is found on contact
 bool isIncontact = false;
+
+// String which holds the last line read from Serial activitiy
+String lastLine = "";
+
+// To be saved phonemumber who sent SMS
+String phonenum = "";
+
+// Integer indexes
+int firstComma = -1;
+int secondComma = -1;
+int thirdComma = -1;
+int forthComma = -1;
+int fifthComma = -1;
+int firstQuote = -1;
+int secondQuote = -1;
+int len = -1;
+int j = -1;
+int i = -1;
+int f = -1;
+int r = -1;
+
+//Boolean to be set to true if number is found on phonebook
+bool isInPhonebook = false;
+
 
 void setup() {
 
@@ -48,61 +75,18 @@ void setup() {
 
 void loop()
 {
+  
   //If there is serial output from SIM800
   if (Serial.available() > 0)
   {
     char lastCharRead = Serial.read();
+
     //Read each character from serial output until \r or \n is reached (which denotes end of line)
     if (lastCharRead == '\r' || lastCharRead == '\n')
     {
-      String lastLine = String(currentLine);
-
-      //If last line read +CMT, New SMS Message Indications was received.
-      //Hence, next line is the message content.
-      if (lastLine.startsWith("+CMT:"))
-      {
-
-        Serial.println(lastLine);
-        nextLineIsMessage = true;
-      }
-      else if (lastLine.length() > 0)
-      {
-        if (nextLineIsMessage)
-        {
-          Serial.println(lastLine);
-
-          //Read message content and set status according to SMS content
-          if (lastLine.indexOf("LED ON") >= 0)
-          {
-            //ledStatus = 1;
-            digitalWrite(LED_BUILTIN, LOW);
-            //sendSMS("04168262667", "LED IS ON");
-            clearBuffer();
-          }
-          else if (lastLine.indexOf("LED OFF") >= 0)
-          {
-            //ledStatus = 0;
-            digitalWrite(LED_BUILTIN, HIGH);
-            //sendSMS("04168262667", "LED IS OFF");
-            clearBuffer();
-          }
-          else
-          {
-            clearBuffer();
-          }
-
-          nextLineIsMessage = false;
-        }
-
-      }
-
-      //Clear char array for next line of read
-      for ( int i = 0; i < sizeof(currentLine);  ++i )
-      {
-        currentLine[i] = (char)0;
-      }
-      currentLineIndex = 0;
+      endOfLineReached();
     }
+
     else
     {
       currentLine[currentLineIndex++] = lastCharRead;
@@ -218,3 +202,127 @@ void clearBuffer()
     delay(1);
   }
 }
+/////////////////////////////////////////////////////////////////////////////////
+void endOfLineReached()
+{
+  lastLine = String(currentLine);
+
+  //-----------------------Call---------------------------//
+  // If lastLine reads RING, New Call Indication was received.
+  // Hence, the THIRD LINE is the caller information.
+
+  // The whole string will be something like this if caller IS registered on SIM Card:
+  //RING (First Line)
+  //     (Second Line is empty)
+  //+CLIP: "04168262667",129,"",0,"Yoimer",0 (Third Line)
+
+  // The whole string will be something like this if caller is NOT registered on SIM Card
+  //RING (First Line)
+  //     (Second Line is empty)
+  //+CLIP: "04168262667",129,"",0,"",0 (Third Line)
+
+  //----------------------SMS-------------------------------------//
+  //If lastLine reads +CMT, New SMS Message Indications was received.
+  //Hence, next line is the message content.
+
+  if (lastLine.startsWith("RING"))                                   // New incoming call
+  {
+    Serial.println(lastLine);
+    nextValidLineIsCall = true;
+  }
+  else if ((lastLine.length() > 0) && (nextValidLineIsCall))        // Rejects any empty line
+  {
+    ////LastLineIsCLIP();
+  }
+  else if (lastLine.startsWith("+CMT:"))                           // New incoming SMS
+  {
+    Serial.println(lastLine);
+    phonenum = lastLine.substring((lastLine.indexOf(34) + 1),   //Parser to extract phonenumber
+                                  lastLine.indexOf(34, lastLine.indexOf(34) + 1));
+    ////Serial.println(phonenum);
+    nextLineIsMessage = true;
+
+    // Parsing lastLine to determine registration on SIM card
+    firstComma = lastLine.indexOf(',');
+    Serial.println(firstComma);  //For debugging
+    secondComma = lastLine.indexOf(',', firstComma + 1);
+    Serial.println(secondComma); //For debugging
+
+    // +CMT: "+584168262667","","17/03/14,16:18:53-16" TelefÃ³nica Movistar Venezuela with no contacts saved
+    //firstComma = 21
+    //secondComma = 24
+
+    Serial.println("Diff");
+    Serial.println(secondComma - firstComma);
+
+
+  }
+  else if ((lastLine.length() > 0) && (nextLineIsMessage))       // Rejects any empty line
+  {
+
+    LastLineIsCMT();
+
+  }
+
+  CleanCurrentLine();
+}
+/////////////////////////////////////////////////////////////////////
+void CleanCurrentLine()
+{
+  //Clear char array for next line of read
+  for ( int i = 0; i < sizeof(currentLine);  ++i )
+  {
+    currentLine[i] = (char)0;
+  }
+  currentLineIndex = 0;
+}
+//////////////////////////////////////////////////////////////////////
+void LastLineIsCMT()
+{
+  if (nextLineIsMessage)
+  {
+    Serial.println(lastLine);
+
+
+    // If exists on Phonebook
+    //if (secondComma > 22)   // Only works with Movilnet and Digitel Venezuela
+    //if (secondComma > 24)    // Only works with for TelefÃ³nica Movistar Venezuela
+    if ((secondComma - firstComma) > 3)
+    {
+      Serial.println("In Phonebook"); //For debugging
+      isInPhonebook = true;
+      Serial.println(isInPhonebook);
+      clearBuffer();
+    }
+    else
+    {
+      Serial.println("Not in Phonebook"); //For debugging
+      isInPhonebook = false;
+      clearBuffer();
+    }
+
+    //if on phonebook ---------------------------
+    if (isInPhonebook)
+    {
+      // If SMS contains LED ON or LED OFF or #WhiteList
+      if (lastLine.indexOf("LED ON") >= 0)
+      {
+         digitalWrite(LED_BUILTIN, LOW);  // Turns ON LED
+         clearBuffer();
+      }
+      else if (lastLine.indexOf("LED OFF") >= 0)
+      {
+         digitalWrite(LED_BUILTIN, HIGH);  // Turns OFF LED
+         clearBuffer();
+      }
+      else
+      {
+        clearBuffer();
+      }
+    }
+    CleanCurrentLine();
+    nextLineIsMessage = false;
+  }
+}
+//////////////////////////////////////////////////////////////////////////
+
