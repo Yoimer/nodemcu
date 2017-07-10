82ba02e3ef028327b342a2b998ede42cdc0eef22
Yoimer <ydrf19@gmail.com>
2017-07-10 13:52:05 -0400

    Added SD card file dump

--- nodemcu-SD/nodemcu-SD.ino
+++ nodemcu-SD/nodemcu-SD.ino
@@ -55,7 +55,8 @@ void setup()
 
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
-  File dataFile = SD.open("datalog.txt");
+  //File dataFile = SD.open("datalog.txt");
+  File dataFile = SD.open("TEST.txt");
 
   // if the file is available, write to it:
   if (dataFile) {
