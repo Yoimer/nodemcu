c2a7a10ee8941eb3e21c48e8cbb7c1aa6780bead
Yoimer <ydrf19@gmail.com>
2017-11-24 23:04:11 -0400

    removed User-agent

--- nodemcu-alexa/nodemcu-alexa.ino
+++ nodemcu-alexa/nodemcu-alexa.ino
@@ -64,7 +64,6 @@ void loop() {
 
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" +
-               "User-Agent: BuildFailureDetectorESP8266\r\n" +
                "Connection: close\r\n\r\n");
 			   
   Serial.println("request sent");
