efe2d1d05e687504d02c1f515f1df9b8a5230ea7
Yoimer <ydrf19@gmail.com>
2018-02-09 22:36:34 -0400

    get led value from botton on ubidot

--- ubidots/ubidots.ino
+++ ubidots/ubidots.ino
@@ -1,7 +1,7 @@
 #include "UbidotsMicroESP8266.h"
 
 #define TOKEN  "A1E-6TwRB1I2osbXqKcuJYG3WduSoQG5oI"  // Put here your Ubidots TOKEN
-#define ID_1 "5a7e4c7b7625427107ab3c78" /* Put your variable ID here */
+#define ID_1 "5a7e5757c03f9767bc12fedf" /* Put your variable ID here */
 #define WIFISSID "Casa" // Put here your Wi-Fi SSID
 #define PASSWORD "remioy2006202" // Put here your Wi-Fi password
 
@@ -11,10 +11,14 @@ void setup() {
     Serial.begin(115200);
     delay(10);
     client.wifiConnection(WIFISSID, PASSWORD);
+    //To change the Device Name:
+    client.setDataSourceName("NODEMCU");
+    //To change the Device label:
+    client.setDataSourceLabel("elapsed time");
 }
 
 void loop() {
-    float value = client.getValue(ID_1);
+    bool value = client.getValue(ID_1);
     Serial.print("Value: ");
     Serial.println(value);
     delay(10000);
