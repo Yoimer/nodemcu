509d466a94c7907e8622c9647c91159f3563d267
Yoimer <ydrf19@gmail.com>
2018-02-09 22:07:33 -0400

    send values using the variable ID

--- ubidots/ubidots.ino
+++ ubidots/ubidots.ino
@@ -1,6 +1,7 @@
 #include "UbidotsMicroESP8266.h"
 
 #define TOKEN  "A1E-6TwRB1I2osbXqKcuJYG3WduSoQG5oI"  // Put here your Ubidots TOKEN
+#define ID_1 "5a7e4c7b7625427107ab3c78" /* Put your variable ID here */
 #define WIFISSID "Casa" // Put here your Wi-Fi SSID
 #define PASSWORD "remioy2006202" // Put here your Wi-Fi password
 
@@ -10,18 +11,13 @@ void setup(){
     Serial.begin(115200);
     client.wifiConnection(WIFISSID, PASSWORD);
     //client.setDebug(true); // Uncomment this line to set DEBUG on
-    //To change the Device Name:
-    client.setDataSourceName("NODEMCU");
-    //To change the Device label:
-    client.setDataSourceLabel("elapsed time"); 
-}
+
 void loop(){
-    //float value1 = analogRead(0);
+    // float value1 = analogRead(A0);
+    /* float value2 = digitalRead(D1) */
+    // client.add(ID_1, value1);
+    /* client.add(ID_2, value2); */
     float value1 = millis();
-    //float value2 = analogRead(2)
-    //client.add("temperature", value1);
-    //client.add("switch", value2);
-    client.add("time", value1);
-    client.sendAll(true);
-    delay(5000);
+    client.add(ID_1, value1);
+    client.sendAll(false);
 }
