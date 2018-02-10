2efcde29dbbf687fb39616b050146b7b574b6b30
Yoimer <ydrf19@gmail.com>
2018-02-09 22:19:32 -0400

    get value from ubidots using client.get.Value(ID_1)

--- ubidots/ubidots.ino
+++ ubidots/ubidots.ino
@@ -7,17 +7,15 @@
 
 Ubidots client(TOKEN);
 
-void setup(){
+void setup() {
     Serial.begin(115200);
+    delay(10);
     client.wifiConnection(WIFISSID, PASSWORD);
-    //client.setDebug(true); // Uncomment this line to set DEBUG on
+}
 
-void loop(){
-    // float value1 = analogRead(A0);
-    /* float value2 = digitalRead(D1) */
-    // client.add(ID_1, value1);
-    /* client.add(ID_2, value2); */
-    float value1 = millis();
-    client.add(ID_1, value1);
-    client.sendAll(false);
+void loop() {
+    float value = client.getValue(ID_1);
+    Serial.print("Value: ");
+    Serial.println(value);
+    delay(10000);
 }
