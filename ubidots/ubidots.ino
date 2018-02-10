d38d2ffcc93c2f56d8bdf530704532ff5130eba3
Yoimer <ydrf19@gmail.com>
2018-02-09 23:10:24 -0400

    control built-in led from botton on ubidots

--- ubidots/ubidots.ino
+++ ubidots/ubidots.ino
@@ -1,25 +1,43 @@
 #include "UbidotsMicroESP8266.h"
 
-#define TOKEN  "A1E-6TwRB1I2osbXqKcuJYG3WduSoQG5oI"  // Put here your Ubidots TOKEN
-#define ID_1 "5a7e5757c03f9767bc12fedf" /* Put your variable ID here */
-#define WIFISSID "Casa" // Put here your Wi-Fi SSID
-#define PASSWORD "remioy2006202" // Put here your Wi-Fi password
+#define TOKEN "A1E-6TwRB1I2osbXqKcuJYG3WduSoQG5oI"  // put here your Ubidots TOKEN
+#define ID_1 "5a7e5757c03f9767bc12fedf" /* put your variable ID here */
+#define WIFISSID "Casa" // put here your Wi-Fi SSID
+#define PASSWORD "remioy2006202" // put here your Wi-Fi password
 
 Ubidots client(TOKEN);
 
 void setup() {
+    // initialize the LED_BUILTIN pin as an output
+    pinMode(LED_BUILTIN, OUTPUT);     
     Serial.begin(115200);
     delay(10);
     client.wifiConnection(WIFISSID, PASSWORD);
-    //To change the Device Name:
+    //to change the device name:
     client.setDataSourceName("NODEMCU");
-    //To change the Device label:
+    //to change the device label:
     client.setDataSourceLabel("elapsed time");
 }
 
-void loop() {
-    bool value = client.getValue(ID_1);
-    Serial.print("Value: ");
-    Serial.println(value);
-    delay(10000);
+void loop()
+{
+    bool led_status = client.getValue(ID_1);
+    Serial.print("led_status: ");
+    Serial.println(led_status);
+    switch (led_status)
+    {
+        case 0:
+            // turn the LED on (Note that LOW is the voltage level
+            // but actually the LED is on; this is because 
+            // it is active low on the ESP-01)
+            digitalWrite(LED_BUILTIN, HIGH);
+            break;
+        case 1:
+            // turn the LED off by making the voltage HIGH
+            digitalWrite(LED_BUILTIN, LOW);
+            break;
+        default:
+            break;
+    }
+    delay(2000);
 }
